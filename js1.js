8c932c0988b379964cf90304218714cbbf489dfa
aidenbarger <student@bcdm52557.local>
2016-11-04 10:52:12 -0400

    added semicolons

--- js1.js
+++ js1.js
@@ -4,4 +4,13 @@ $( document ).ready(function(){
 			$('h3').css('background-color', '#FFFF00');
 		}
 		);
+	$('a').click(
+		function(){
+			$('a').css('display', 'none');
+		})
+	$('body').mousemove(
+		function(){
+			$('body').css('position', 'relative');
+			$('body').css('left', '30px');
+		})
 });
